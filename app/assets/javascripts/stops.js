d6e34e0e5f5ee85313525cefcd948e66e5ffcda0
Aman Kapur <amankapur91@gmail.com>
2012-07-11 21:14:04 -0400

    v6

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -1,19 +1,13 @@
 $(document).on('ready', function(){
-	var client_ip;
-	$.getJSON("http://smart-ip.net/geoip-json?callback=?",
-    function(data){
-       client_ip = data.ip;
-  });
-  var client_lat;
+	var client_lat;
   var client_lon;
-  alert(client_ip);
-  $.get("http://api.hostip.info/get_html.php?ip=" + client_ip + "&position=true", 
-  		
-  		function(data) {
-  			client_lat = data.lat
-  			client_lon = data.lng
-  		});
-  		
+  $.getJSON("http://smart-ip.net/geoip-json?callback=?",
+    function(data){
+       client_lat = data.latitude;
+       client_lon = data.longitude;
+  });
+
+  
 	var cloudmade = new CM.Tiles.CloudMade.Web({key: '7324c064b04d402eb2a58d68c935b63d'});
   var map = new CM.Map('cm-example', cloudmade);
   map.setCenter(new CM.LatLng(client_lat, client_lon), 15);
